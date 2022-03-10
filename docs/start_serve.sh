b80517a5c70e4e6d10a83033099d3dc00f085c9e
mruizq <mruizq@unah.hn>
2022-03-10 04:11:24 -0600

    slide-box in home

--- docs/start_serve.sh
+++ docs/start_serve.sh
@@ -3,4 +3,4 @@ source /usr/local/rvm/scripts/rvm
 rvm use 2.7.2
 cd /home/docs/
 bundler install
-bundle exec jekyll serve -H 0.0.0.0 --livereload
+bundle exec jekyll serve -H 0.0.0.0 --livereload --incremental
