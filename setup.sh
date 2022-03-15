3d84188678a07cb658a457135485d631afc269d6
aosp <aosp@aosp.aosp>
2022-03-15 15:37:33 -0500

    Creation article 1

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo docker build . -t ubuntu-jekyll
-sudo docker run -v $PWD/docs:/home/docs -p "3000:4000" -it ubuntu-jekyll ./home/docs/start_serve.sh
+sudo docker run -v $PWD/docs:/home/docs -p "4000:4000" -it ubuntu-jekyll ./home/docs/start_serve.sh
 
